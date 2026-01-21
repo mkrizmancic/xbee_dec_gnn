92d07dfdad1bbfb4779486d7a5cc48287045eded
Marko Krizmancic <marko.krizmancic@fer.hr>
2026-01-21 14:51:51 +0100

    Docker run: allow usb device, improve screen and ssh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -29,19 +29,40 @@ if [ "$IS_RPI" = true ]; then
     $IMAGE_NAME
 else
   echo "Running in default mode..."
+
+  # Set up X11 forwarding (display)
+  XAUTH=/tmp/.docker.xauth
+  XSOCK=/tmp/.X11-unix
+  xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+  if [ ! -f $XAUTH ]; then
+      if [ ! -z "$xauth_list" ]; then
+          echo $xauth_list | xauth -f $XAUTH nmerge -
+      else
+          touch $XAUTH
+      fi
+      chmod a+r $XAUTH
+  fi
+
+  # Set up SSH agent forwarding
+  ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+
   docker run \
     -it \
     --network host \
     --ipc host \
-    --volume ~/.ssh/ssh_auth_sock:/ssh-agent \
-    --volume /var/run/dbus:/var/run/dbus \
+    --env TERM=xterm-256color \
+    --volume ~/.ssh/ssh_auth_sock:/run/ssh-agent \
+    --env SSH_AUTH_SOCK=/run/ssh-agent \
     --volume /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket \
+    --volume=$XSOCK:$XSOCK:rw \
+    --volume=$XAUTH:$XAUTH:rw \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env DISPLAY=$DISPLAY \
+    --device-cgroup-rule='c 188:* rmw' \
+    --device-cgroup-rule='c 166:* rmw' \
+    --volume /dev:/dev \
     --volume $SCRIPT_DIR/volumes/datasets:/root/resources/data \
     --volume $SCRIPT_DIR/volumes/models:/root/resources/models \
-    --volume /tmp/.x11-unix:/tmp/.x11-unix \
-    --env SSH_AUTH_SOCK=/ssh-agent \
-    --env DISPLAY=$DISPLAY \
-    --env TERM=xterm-256color \
     --name $CONTAINER_NAME \
     $IMAGE_NAME
 fi
